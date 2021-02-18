fc8d4c10bc44f366c2a9767d6af268b347dd84f6
wonjun0120 <wonjundero@gmail.com>
2021-02-18 11:16:53 +0900

    rollback __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from config import SQLALCHEMY_DATABASE_URI
+
 app = Flask(__name__)
 
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ['SECRET']
 db = SQLAlchemy(app)
-engine = db.create_engine(SQLALCHEMY_DATABASE_URI)
-db.create_all(engine)
+
+db.create_all()
 from app import views, models
